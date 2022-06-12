6854232c46b9e8bb62931b5cdebc27af9a1639ea
jose <lenadrojose124leandro@gmail.com>
2022-06-12 11:48:26 -0300

    comecando e ja quase terminado o jogo

---
+++ java/carregamentoinicial.js
@@ -0,0 +1,61 @@
+var botaoini = document.querySelector(".botaoini");
+botaoini.addEventListener("click", function(event){
+    event.preventDefault();
+});
+
+  var criarbotaonovojogo = document.querySelector(".botaonovojogo");
+    criarbotaonovojogo.style.visibility = "hidden";
+    var criarbotaodesistir = document.querySelector(".botaodesitir");
+    criarbotaodesistir.style.visibility = "hidden";
+
+function iniciarosrecusosdojogo() {
+ 	      
+        var titulodojogo = document.querySelector("#titulo");
+        titulodojogo.classList.add("titulo");
+        var nomedotitulo = "jogando um joginho";
+        titulodojogo.textContent = nomedotitulo;
+
+        var botaoini = document.querySelector(".botaoini");
+        botaoini.style.display = "none";
+        var botaoadi = document.querySelector(".botaoadi");
+        botaoadi.style.display = "none";
+
+        var criarbotaonovojogo = document.querySelector(".botaonovojogo");
+        criarbotaonovojogo.style.visibility = "visible";
+        var criarbotaodesistir = document.querySelector(".botaodesitir");
+        criarbotaodesistir.style.visibility = "visible";
+            
+        var tela = document.querySelector("canvas");
+        tela.classList.add("tamanho");           
+       
+         
+         var iniciarosrecusosdojogo = colocarbotoesrestantes();
+         var iniciarosrecusosdojogo = sorteiapalavras();
+       
+         var iniciarosrecusosdojogo = desenhalinhas();
+ }
+
+ function colocarbotoesrestantes() {
+    
+
+    criarbotaonovojogo.classList.add("botaonovojogo");
+    var criarp = document.querySelector(".novojogo");
+    criarp.textContent = "novo jogo";
+
+    var criarbotaodesistir = document.querySelector(".botaodesitir");
+    criarbotaodesistir.classList.add("botaodesistir");
+    var criarpdesistir = document.querySelector(".desistir");
+    criarpdesistir.textContent = "desistir";
+    
+ }
+            
+ 
+
+
+
+
+
+
+
+
+
