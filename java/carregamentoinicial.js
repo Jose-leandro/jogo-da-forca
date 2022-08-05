02d6e927ad0753f9b01ad448755c786a85e07f93
jose <lenadrojose124leandro@gmail.com>
2022-08-05 16:38:07 -0300

    realizando os últimos detalhes

--- java/carregamentoinicial.js
+++ java/carregamentoinicial.js
@@ -1,65 +1,89 @@
-var botaoini = document.querySelector(".botaoini");
-botaoini.addEventListener("click", function(event){
+//previnir que a página sejá recarregada quando for clicado nos botões
+  function Previnir() {
+    var BtnAdicionarPalavrasNovasNoJogo = document.querySelector(".btn-adicionar");
+    BtnAdicionarPalavrasNovasNoJogo.addEventListener("click", function(event){
+      event.preventDefault();
+    });
+  
+    var BtnVoltar = document.querySelector(".btn-voltar");
+    BtnVoltar.addEventListener("click", function(event){
+      event.preventDefault();
+    });
+
+    var BtnIniciarOJogo = document.querySelector(".btn-iniciar");
+    BtnIniciarOJogo.addEventListener("click", function(event){
+        event.preventDefault();
+    });
+
+    var BtnNovoJogo = document.querySelector(".btn-novojogo")
+    BtnNovoJogo.addEventListener("click", function(event){
+      event.preventDefault();
+    });
+
+    var BtnDesistir = document.querySelector(".btn-desistir");
+    BtnDesistir.addEventListener("click", function(event){
     event.preventDefault();
-});
+    });
+  }Previnir();
 
-  var criarbotaonovojogo = document.querySelector(".botaonovojogo");
-    criarbotaonovojogo.style.visibility = "hidden";
-    var criarbotaodesistir = document.querySelector(".botaodesitir");
-    criarbotaodesistir.style.visibility = "hidden";
+  //o porque, pois eu precisava declarar fora da função para que funcione
+  var MostarBtnNovoJogo = document.querySelector(".btn-novojogo");
+  MostarBtnNovoJogo.style.visibility = "hidden";
 
-function iniciarosrecusosdojogo() {
- 	      
-        var titulodojogo = document.querySelector("#titulo");
-        titulodojogo.classList.add("titulo");
-        var nomedotitulo = "jogando um joginho";
-        titulodojogo.textContent = nomedotitulo;
+  var MostarBtnDesistir = document.querySelector(".btn-desistir");
+  MostarBtnDesistir.style.visibility = "hidden";
 
-        var imagem = document.querySelector(".imagem-forca");
-        imagem.style.visibility = "visible";
+  //esconde e tambem oculta determinados btns , imgs e funções do jogo
+function CarregarOsRecusosDoJogo() {
+ 	
+  var TituloDoJogo = document.querySelector(".titulo");
+  TituloDoJogo.style.visibility = "visible";
 
-        var botaoini = document.querySelector(".botaoini");
-        botaoini.style.display = "none";
-        var botaoadi = document.querySelector(".botaoadi");
-        botaoadi.style.display = "none";
+  var  MostarBtnIniciarOJogo = document.querySelector(".btn-iniciar");
+  MostarBtnIniciarOJogo.style.visibility = "hidden";
 
-        var criarbotaonovojogo = document.querySelector(".botaonovojogo");
-        criarbotaonovojogo.style.visibility = "visible";
-        var criarbotaodesistir = document.querySelector(".botaodesitir");
-        criarbotaodesistir.style.visibility = "visible";
-            
-        var tela = document.querySelector("canvas");
-        tela.classList.add("tamanho");           
-       
-         
-         var iniciarosrecusosdojogo = colocarbotoesrestantes();
-         var iniciarosrecusosdojogo = sorteiapalavras();
-        var iniciarosrecusosdojogo = tela.onclick = dispara;
-         var iniciarosrecusosdojogo = desenhalinhas();
-         
+  var BtnAdicionarPalavrasNovasNoJogo = document.querySelector(".btn-adicionar");
+  BtnAdicionarPalavrasNovasNoJogo.style.visibility = "hidden";
+
+  var MostarBtnNovoJogo = document.querySelector(".btn-novojogo");
+  MostarBtnNovoJogo.style.visibility = "visible";
+  
+  var MostarBtnDesistir = document.querySelector(".btn-desistir");
+  MostarBtnDesistir.style.visibility = "visible";          
+   
+  var Tela = document.querySelector("canvas");
+  Tela.classList.add("tela"); 
+
+  var Canvas = document.querySelector("#canvas");
+  Canvas.style.visibility = "visible";
+     
+  DesenhaTracos()
+  DetectarLetrasQuandoPressionada()
  }
 
- function colocarbotoesrestantes() {
-    
 
-    criarbotaonovojogo.classList.add("botaonovojogo");
-    var criarp = document.querySelector(".novojogo");
-    criarp.textContent = "novo jogo";
+ //deixar visivel quando for clicado sobre o simbolo de direitos autorais
+ function DireitosAutorais() {
+    var SimboloDireitos = document.querySelector(".material-symbols-outlined");
+      SimboloDireitos.addEventListener("click", function(){
+        var MostarContenerDireitos = document.querySelector(".direitos");
+        MostarContenerDireitos.style.visibility = " visible";
 
-    var criarbotaodesistir = document.querySelector(".botaodesitir");
-    criarbotaodesistir.classList.add("botaodesistir");
-    var criarpdesistir = document.querySelector(".desistir");
-    criarpdesistir.textContent = "desistir";
-    
- }
-            
- 
+        var MostarContenerInformacaoDireitos = document.querySelector(".informacao-direitos");
+        MostarContenerInformacaoDireitos.style.visibility = "visible";
+        MostarContenerInformacaoDireitos.classList.add("direitos");
+      });
 
+ }DireitosAutorais();
 
-
-
-
-
-
-
-
+  //esconder as informações quando for  clicado duas vezes no simbolo 
+ function EsconderDireitosAutorais() {
+    var SimboloDireitosAutorais = document.querySelector(".material-symbols-outlined");
+    SimboloDireitosAutorais.addEventListener("dblclick", function() {
+      var EsconderContenerDireitos = document.querySelector(".direitos");
+      EsconderContenerDireitos.style.visibility = " hidden";
+      
+      var MostarContenerInformacaoDireitos = document.querySelector(".informacao-direitos");
+      MostarContenerInformacaoDireitos.style.visibility = "hidden";
+    });
+ }EsconderDireitosAutorais();
